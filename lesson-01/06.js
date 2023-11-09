058f95fff0a5b0b9f0fee9ce527540eacf25717c
andrey0533 <l-2019@mail.ru>
2023-11-09 20:53:33 +0300

    make lesson 01-06

---
+++ lesson-01/06.js
@@ -0,0 +1,9 @@
+let str = "zzzzyzz";
+let strArray = Array.from(str);
+
+console.log(strArray.includes('y') ? 'нашел' : 'заблудился')
+
+//console.log(strArray.includes);
+//const num = 5
+//console.log(num === 5 ? 'Пять' : 'Не пять')
+// Пять
